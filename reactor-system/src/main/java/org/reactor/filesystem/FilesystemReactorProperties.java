9862cb02d3a35895f694fc06542336c75fe9c58c
Tomasz Kuryłek <tkurylek@future-processing.com>
2014-12-10 12:06:53 +0100

    Reactor-boostrap generates windows scripts.
    - By default reactor filesystem directory is set to system's temp
      directory. This can be easily overriden by reactor.filesystem.directory
      property.
    
    Code review:
    - Corrected system temporary directory constant

--- reactor-system/src/main/java/org/reactor/filesystem/FilesystemReactorProperties.java
+++ reactor-system/src/main/java/org/reactor/filesystem/FilesystemReactorProperties.java
@@ -3,21 +3,22 @@ package org.reactor.filesystem;
 import static java.lang.Boolean.TRUE;
 import static java.lang.Boolean.parseBoolean;
 
-import org.reactor.ReactorProperties;
-
 import java.util.Properties;
+
+import org.reactor.ReactorProperties;
 
 public class FilesystemReactorProperties extends ReactorProperties {
 
     public static final String PROPERTY_LISTENING_DIRECTORY = "reactor.filesystem.directory";
     public static final String PROPERTY_LISTENER_ENABLED = "reactor.filesystem.listener.enabled";
+    public static final String PROPERTY_SYSTEM_TEMPORARY_DIRECTORY = "java.io.tmpdir";
 
     public FilesystemReactorProperties(Properties properties) {
         super(properties);
     }
 
     public String getListeningDirectory() {
-        return getProperty(PROPERTY_LISTENING_DIRECTORY);
+        return getProperty(PROPERTY_LISTENING_DIRECTORY, System.getProperty(PROPERTY_SYSTEM_TEMPORARY_DIRECTORY));
     }
 
     public boolean isListenerEnabled() {
