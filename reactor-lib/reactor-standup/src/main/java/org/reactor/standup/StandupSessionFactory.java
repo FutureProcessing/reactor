9663bc88943192196bdc8dfed79057e534d5c7a9
activey <activey@doaplatform.org>
2014-12-17 16:01:34 +0100

    Removed standup reactor, maybe some other time ;)

--- reactor-lib/reactor-standup/src/main/java/org/reactor/standup/StandupSessionFactory.java
+++
@@ -1,8 +0,0 @@
-package org.reactor.standup;
-
-public class StandupSessionFactory {
-
-    public StandupSession newSession(String sessionOwner) {
-        return new StandupSession(sessionOwner);
-    }
-}
